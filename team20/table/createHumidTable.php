4269ba34bca72c20fd58d8c3496febf77f7c5e26
MinJi.CH <81309465+SUMMERLOVE7@users.noreply.github.com>
2022-11-14 01:11:20 +0900

    Code for creating tables

--- team20/table/createHumidTable.php
+++ team20/table/createHumidTable.php
@@ -1,13 +1,13 @@
 <?php
     //include("login.php");
-   
-    $sql = "CREATE TABLE RAINTABLE (raindate INT(8) NOT NULL PRIMARY KEY AUTO_INCREMENT, regioncode VARCHAR(10), rainrate NUMERIC)";
+
+    $sql = "CREATE TABLE HUMIDTABLE (humiddate INT(8), regioncode VARCHAR(10), humidrate NUMERIC)";
 
     $res = mysqli_query($mysqli, $sql);
     
     if($res === TRUE){
         //echo "Tables are successfully created";
-        print("rain");
+        print("Humid");
     }
     else{
         printf("Could not create table:%s\n", mysqli_err($mysqli));
